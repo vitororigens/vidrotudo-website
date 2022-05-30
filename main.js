d439d091dea2bf9fb91c20625841ea128665b9f5
vitororigens <vitorabreufjb@hotmail.com>
2022-05-30 10:09:38 -0300

    transição de texto

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ function toggleMenu(event){
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
+
+
+var i = 0;
+// var A = 0;
+    var tag = document.getElementById("text");
+    var html = document.getElementById("text").innerHTML;
+    var attr = tag.setAttribute("data", html);
+    var txt = tag.getAttribute("data");
+    var speed = 170;
+
+    function typeWriter() {
+      if (i <= txt.length) {
+        document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+        //console.log(document.getElementById("text").innerHTML);
+    }
+
+typeWriter();
